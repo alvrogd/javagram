784ab14813932c44f089569068c4d297ade1f6ed
Álvaro <alvarogoldard@gmail.com>
2020-03-30 20:15:37 +0200

    Initial commit. Project configuration

---
+++ src/main/java/com/goldardieste/javagram/server/RunServer.java
@@ -0,0 +1,13 @@
+package com.goldardieste.javagram.server;
+
+import java.io.IOException;
+
+public class RunServer {
+
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("Hey there");
+
+        System.in.read();
+    }
+}
